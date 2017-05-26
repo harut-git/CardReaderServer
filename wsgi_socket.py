457922160464dfca3219612db8ffd93c855a1c19
Harut <harut.ghubasaryan@gmail.com>
2017-05-26 20:03:19 +0400

    conn commit

--- wsgi_socket.py
+++ wsgi_socket.py
@@ -15,41 +15,44 @@ class MyWebSocket(WebSocket):
     read_id = None
 
     def opened(self):
-        if self.opened:
-            self.read_id = CardReader.reader()
-            self.read_id = re.sub('[a-z]', '', self.read_id)
-            print self.read_id
-            if not check_if_emp_exists(self.read_id):
-                response = {'command': "add_employee",
-                            'params': {
-                                'entry_id': self.read_id
-                            }}
-                self.send(json.dumps(response))
-            else:
-                self.send(json.dumps({'command': "employee_exists",
-                                      'params': {
-                                          'entry_id': self.read_id
-                                      }}))
-        else:
-            self.close_connection()
+        print "Connection opened"
+
+    def closed(self, code, reason=None):
+        self.close_connection()
 
     def received_message(self, message):
-        request = json.loads(str(message))
-        command = request['command']
-        params = request['params']
-        if command not in available_commands:
-            response = {
-                "status": "false",
-                "error": 3
-            }
+        if type(message) == unicode:
+            while 1:
+                self.read_id = CardReader.reader()
+                self.read_id = re.sub('[a-z]', '', self.read_id)
+                print self.read_id
+                if not check_if_emp_exists(self.read_id):
+                    response = {'command': "add_employee",
+                                'params': {
+                                    'entry_id': self.read_id
+                                }}
+                    self.send(json.dumps(response))
+                else:
+                    self.send(json.dumps({'command': "employee_exists",
+                                          'params': {
+                                              'entry_id': self.read_id
+                                          }}))
+        else:
+            request = json.loads(str(message))
+            command = request['command']
+            params = request['params']
+            if command not in available_commands:
+                response = {
+                    "status": "false",
+                    "error": 3
+                }
+                self.send(json.dumps(response), message.is_binary)
+            module = __import__('socket_commands')
+            attr = getattr(module, command)
+            response = attr(params=params)
+            print "response: ", response
             self.send(json.dumps(response), message.is_binary)
-        module = __import__('socket_commands')
-        attr = getattr(module, command)
-        response = attr(params=params)
-        print "response: ", response
-        self.send(json.dumps(response), message.is_binary)
-        print self.environ
-
+            print self.environ
 
 server = WSGIServer(('localhost', 8787), WebSocketWSGIApplication(handler_cls=MyWebSocket))
 server.serve_forever()
