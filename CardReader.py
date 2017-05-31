8952fbcde1711b609a4f4f6576d061678dfd9657
harutyun <harutyun.ghubasaryan@betconstruct.com>
2017-05-31 15:11:51 +0400

    card reader to file

--- CardReader.py
+++ CardReader.py
@@ -1,30 +1,31 @@
 import serial
 
-
-def reader(ser, close=True):
-    try:
-        ser.isOpen()
-        print "Serial port is open"
-    except Exception as e:
-        print ("Error")
-        print e.message
-        exit()
-    if ser.isOpen():
-        try:
-            while close:
-                entry_id = ser.read(size=10)
-                print type(entry_id)
-                return entry_id
-        except Exception as e:
-            print "Error on reading"
-            print e.message
-    else:
-        print "Cannot open Serial Port"
+# ser = serial.Serial(port='COM1', baudrate='9600')
 
 
-def tester():
-    ser = serial.Serial(port='COM1', baudrate='9600')
-    k = reader(ser)
-    ser.close()
-    print "port closed"
-    print k
+def reader():
+    while 1:
+        k = input()
+        with open("current.txt", 'w') as outfile:
+            outfile.write(k)
+    # try:
+    #     ser.isOpen()
+    #     print "Serial port is open"
+    # except Exception as e:
+    #     print ("Error")
+    #     print e.message
+    #     exit()
+    # if ser.isOpen():
+    #     try:
+    #         while True:
+    #             entry_id = ser.read(size=10)
+    #             with open("current.txt", 'w') as outfile:
+    #                 outfile.write(entry_id)
+    #     except Exception as e:
+    #         print "Error on reading"
+    #         print e.message
+    # else:
+    #     print "Cannot open Serial Port"
+
+
+reader()
--- socket_commands.py
+++ socket_commands.py
@@ -1,3 +1,5 @@
+import re
+
 from DbConnect import db_conn
 
 
@@ -27,13 +29,14 @@ def add_employee(params):
     con.set_character_set('utf8')
     cur = con.cursor()
     cur.execute("INSERT INTO employees VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (img_url,
-                                                                                                         entry_id, name,
-                                                                                                         sur_name,
-                                                                                                         position,
-                                                                                                         group,
-                                                                                                         start_ts,
-                                                                                                         end_ts,
-                                                                                                         comment))
+                                                                                                               entry_id,
+                                                                                                               name,
+                                                                                                               sur_name,
+                                                                                                               position,
+                                                                                                               group,
+                                                                                                               start_ts,
+                                                                                                               end_ts,
+                                                                                                               comment))
 
 
 def update_employee(params):
@@ -44,3 +47,22 @@ def update_employee(params):
     position = params['position']
     start_ts = params['start_ts']
     end_ts = params['end_ts']
+
+
+def listen_reader(params):
+    read_file = open("current.txt", "r")
+    read_id = read_file.read()
+    read_id = re.sub('[a-z]', '', read_id)
+    print read_id
+    if not check_if_emp_exists(read_id):
+        response = {'command': "add_employee",
+                    'params': {
+                        'entry_id': read_id
+                    }}
+        return response
+    else:
+        response = {'command': "employee_exists",
+                    'params': {
+                        'entry_id': read_id
+                    }}
+        return response
